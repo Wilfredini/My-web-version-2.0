96ab5287391958b341b2a2b2f284399b7b39a1bb
Tomas Charvat <charvi.tomas@seznam.cz>
2022-10-04 19:57:01 +0200

    menu modifier

--- script.js
+++ script.js
@@ -8,8 +8,13 @@ console.log(menuLink);
 menuLink.forEach((element) => {
   element.addEventListener("click", function () {
     menuLink.forEach((item) => item.classList.remove("active"));
-
+    setTimeout(function () {
+      if (menuOpen) {
+        menu.classList.remove("open");
+      }
+    }, 1000);
     this.classList.add("active");
+    menuBtn.classList.remove("open");
   });
 });
 
@@ -48,3 +53,34 @@ themeMenuBtn.addEventListener("click", () => {
     themeMenuOpen = false;
   }
 });
+
+/* -------------------------------------------------------------------------- */
+/*                              Smooth scrolling                              */
+/* -------------------------------------------------------------------------- */
+
+function smoothScroll(target, duration) {
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function animationScroll(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animationScroll);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animationScroll);
+}
+
+smoothScroll(".about", 1000);

a3d67c6c4c136437ec52ff3bed98a1159e458330
Tomas Charvat <charvi.tomas@seznam.cz>
2022-10-13 11:56:07 +0200

    Finish changes-frontend

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 /* -------------------------------------------------------------------------- */
 /*                                 Menu set-up                                */
 /* -------------------------------------------------------------------------- */
-
 const menuLink = document.querySelectorAll(".nav-link");
-console.log(menuLink);
 
 menuLink.forEach((element) => {
   element.addEventListener("click", function () {
     menuLink.forEach((item) => item.classList.remove("active"));
     setTimeout(function () {
-      if (menuOpen && window.innerWidth < 405) {
+      if (menuOpen && window.innerWidth < 711) {
         menu.classList.remove("open");
       }
     }, 1000);
@@ -40,7 +38,10 @@ menuBtn.addEventListener("click", () => {
 /* -------------------------------------------------------------------------- */
 /*                                Theme changer                               */
 /* -------------------------------------------------------------------------- */
-
+const neonBtn = document.querySelector(".neon");
+const lightBtn = document.querySelector(".light");
+const darkBtn = document.querySelector(".dark");
+const alternateStyle = document.querySelectorAll(".alternate-style");
 const themeMenuBtn = document.getElementById("theme");
 const themeMenu = document.querySelector(".theme-menu");
 let themeMenuOpen = false;
@@ -53,7 +54,17 @@ themeMenuBtn.addEventListener("click", () => {
     themeMenuOpen = false;
   }
 });
-
+function setActiveStyle(color) {
+  alternateStyle.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+      themeMenu.classList.remove("open");
+      themeMenuOpen = false;
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+}
 /* -------------------------------------------------------------------------- */
 /*                              Smooth scrolling                              */
 /* -------------------------------------------------------------------------- */
@@ -61,8 +72,24 @@ themeMenuBtn.addEventListener("click", () => {
 document.querySelectorAll("a[href^='#']").forEach((link) => {
   link.addEventListener("click", function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
+    document.querySelector(link.getAttribute("href")).scrollIntoView({
       behavior: "smooth",
     });
   });
 });
+
+/* -------------------------------------------------------------------------- */
+/*                                Cookie setup                                */
+/* -------------------------------------------------------------------------- */
+
+const cookieBox = document.querySelector(".cookie-box");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieBox.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed"))
+    cookieBox.classList.add("active");
+}, 2000);
